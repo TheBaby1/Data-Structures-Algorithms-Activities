082f580107530a749e60a93c12f6011708b6bf35
Nikko Ensomo <104424218+TheBaby1@users.noreply.github.com>
2024-06-14 10:55:14 +0800

    Update main.c
    added getEven function call

--- STACKS/main.c
+++ STACKS/main.c
@@ -6,13 +6,14 @@
 
 int main() {
 	
-	StackArrayList S;
+	StackArrayList S = createStack();
 	
-	initStack(&S);
-	
-	push(&S, 10);
-	push(&S, 20);
-	push(&S, 30);
+	push(&S, 21);
+	push(&S, 25);
+	push(&S, 16);
+	push(&S, 23);
+	push(&S, 99);
+	push(&S, 86); 
 
 
 	printf("DISPLAY: \n");
@@ -21,6 +22,12 @@ int main() {
 	
 	printf("VISUALIZE: \n");
 	visualize(S);
+	
+	// Creating a new stack of even numbers from old stack
+	StackArrayList newStack = getEven(&S);
+	
+	printf("Even Numbers Stack: \n");
+	display(newStack);
 		
 	return 0;
 }
